9aaee1734d7a6afc0b99e2156a696d597fdc5000
Lukasz Dyrda <Lukasz.Dyrda@tzmosa.global>
2021-10-05 12:31:53 +0200

    jeje

--- Kod/r11/gra w zycie.py
+++ Kod/r11/gra w zycie.py
@@ -24,14 +24,48 @@ def ustawienia():
 
     wybor = StringVar(okno_glowne) 
     wybor.set('Wybierz wzorzec')
-    opcja = OptionMenu(okno_glowne, wybor, 'Wybierz wzorzec', 'szybowiec', 'działo/szybowiec', 'losowy')
+    opcja = OptionMenu(okno_glowne, wybor, 'Wybierz wzorzec', 'szybowiec', 'działo/szybowiec', 'losowy', command=handler_opcja)
+    
     opcja.config(width=20)
+    
 
     widok_siatki.grid(row=0, columnspan=3, padx=20, pady=20) 
+    widok_siatki.bind('<Button-1>', handler_siatki)
     przycisk_start.grid(row=1, column=0, sticky=W,padx=20, pady=20)
     przycisk_start.bind('<Button-1>', handler_start)
     opcja.grid(row=1, column=1, padx=20)
     przycisk_wyczysc.grid(row=1, column=2, sticky=E, padx=20, pady=20)
+
+def handler_opcja(zdarzenie):
+    global uruchomiony, przycisk_start, wybor
+
+    uruchomiony = False
+    przycisk_start.configure(text='Start')
+
+    wybrany = wybor.get()
+
+    if wybrany == 'szybowiec':
+        model.wczytaj_wzorzec(model.wzorzec_dzialo_szybowiec, 10, 10)
+    elif wybrany == 'działo/szybowiec':
+        model.wczytaj_wzorzec(model.wzorzec_dzialo_szybowiec, 10, 10)
+    elif wybrany == 'losowy':
+        model.losuj(model.model_siatki, model.szer, model.wys)
+
+    aktualizacja()
+
+def handler_siatki(zdarzenie):           # handler otrzymuje zdarzenie jako argument
+    global widok_siatki, wielkosc_komorki
+
+    x = int(zdarzenie.x / wielkosc_komorki)  # mozemy pobrac wspolrzedne x i y klikniecia z obiektu zdarzenie obydwie maja wlasne atrybuty
+    y = int(zdarzenie.x / wielkosc_komorki)  # skala siatki okreslana jest wielkoscia komorki, wiec aby sprawdzic wspolrzedne dzieliym ja przez wielkosc komorki
+
+    if(model.model_siatki[x][y] == 1):  # jezeli biezaca komorka ma wartosc 1 to nadajemy jej wartosc 0 i wywolujemy
+        model.model_siatki[x][y] == 0   # funkcje narysuj_komorke aby pokolorowac na bialo
+        narysuj_komorke(x, y, 'white')
+    else:
+        model.model_siatki[x][y] = 1   # w przeciwnym razie odwrotnie, czarna komorke kolorujemy na bialo
+        narysuj_komorke(x, y, 'black')
+
 
 
 def handler_start(zdarzenie):
--- Kod/r11/model.py
+++ Kod/r11/model.py
@@ -1,48 +1,42 @@
 import random
 
 wys = 100
-szer = 100  
+szer = 100
 
 def losuj(siatka, szer, wys):
-    for i in range(0, wys):
-        for j in range(0, szer):
-            siatka[i][j] = random.randint(0, 1)      
+   for i in range(0, wys):
+       for j in range(0, szer):
+           siatka[i][j] = random.randint(0,1)
 
-model_siatki = [0] * wys  # tworzymy liste o liczbie zer równiej wysokosci
+
+model_siatki = [0] * wys
 nastepny_model_siatki = [0] * wys
-
 for i in range(wys):
-    model_siatki[i] = [0] * szer   # Iterujemy po kazdym elemencie listy model_siatki i zastepujemy je listami o liczbie zer rownej szerokosci
-    nastepny_model_siatki[i] = [0] * szer
-
-
-losuj(model_siatki, szer, wys)
+   model_siatki[i] = [0] * szer
+   nastepny_model_siatki[i] = [1] * szer
 
 def nastepne_pokolenie():
-    global model_siatki, nastepny_model_siatki
+   global model_siatki, nastepny_model_siatki
 
-    for i in range(0, wys):        # iterujemy po kazdej komorce z dwuwymiarowej listy
-        for j in range(0, szer):
-            komorka = 0
-            
-            licznik = policz_sasiadow(model_siatki, i, j)
-
-            if model_siatki[i][j] == 0:
-                if licznik == 3:
-                    komorka = 1
-            elif model_siatki[i][j] == 1:
-                if licznik == 2 or licznik == 3:
-                    komorka = 1
-            nastepny_model_siatki[i][j] = komorka # po obliczeniu stanu komorki zapisujemy stan w biezacej lokalizacji w siatce nastepny_model_siatki
-            
-    tymczasowa = model_siatki
-    model_siatki = nastepny_model_siatki
-    nastepny_model_siatki = tymczasowa  # po obliczeniu wszystkich wartosci nastepny_model_siatki musimy przekazac je zmiennej 
-                                        # model_siatki. W tym celu zamieniamy zmienne miejscami, tak aby model siatki
-                                        # wskazywal dane z siatki nastepny_model_siatki i na odwrot
+   for i in range(0, wys):
+       for j in range(0, szer):
+           komorka = 0
+           licznik = policz_sasiadow(model_siatki, i, j)
 
 
-def policz_sasiadow(siatka, rzad, kolumna): # funkcja przyjmuje siatke oraz rzad i kolumne a zwraca liczbe zywych sasiadow danej komorki
+           if model_siatki[i][j] == 0:
+               if licznik == 3:
+                   komorka = 1 
+           elif model_siatki[i][j] == 1:
+               if licznik == 2 or licznik == 3:
+                   komorka = 1
+           nastepny_model_siatki[i][j] = komorka
+
+   tymczasowa = model_siatki
+   model_siatki = nastepny_model_siatki
+   nastepny_model_siatki = tymczasowa
+
+def policz_sasiadow(siatka, rzad, kolumna):
 
    licznik = 0
    if rzad-1 >= 0:
@@ -62,6 +56,41 @@ def policz_sasiadow(siatka, rzad, kolumna): # funkcja przyjmuje siatke oraz rzad
    if (rzad + 1 < wys) and (kolumna+1 < szer):
        licznik = licznik + siatka[rzad+1][kolumna+1]
    return licznik
+
+wzorzec_szybowiec = [[0, 0, 0, 0, 0],
+                     [0, 0, 1, 0, 0],
+                     [0, 0, 0, 1, 0],
+                     [0, 1, 1, 1, 0],
+                     [0, 0, 0, 0, 0]]
+
+
+wzorzec_dzialo_szybowiec = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def wczytaj_wzorzec(wzorzec, odstep_x=0, odstep_y=0):
+    global model_siatki
+
+    for i in range(0, wys):
+        for j in range(0, szer):
+            model_siatki[i][j] = 0
+
+    j = odstep_y
+
+    for rzad in wzorzec:
+        i = odstep_x
+        for wartosc in rzad:
+            model_siatki[i][j] = wartosc
+            i = i + 1
+        j = j + 1
 
 if __name__ == '__main__':
     nastepne_pokolenie()
