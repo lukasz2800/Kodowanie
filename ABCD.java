a4a50a444c30dda7342606e149cdef69b7d34966
lukasz2800 <90788748+lukasz2800@users.noreply.github.com>
2021-10-11 18:25:27 +0200

    Add files via upload

---
+++ ABCD.java
@@ -0,0 +1,18 @@
+public class ABCD {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x > 2);
+        System.out.print("A");
+        x = (x - 1);
+        System.out.print("-");
+        x = (x - 1);
+        System.out.print("b-c");
+        x = (x - 1);
+        System.out.println("-d");
+        System.out.print("Trzymaj sie trolku");
+        
+
+
+    }
+    
+}
---
+++ class MojaPierwszaAplikacja {.java
@@ -0,0 +1,6 @@
+class MojaPierwszaAplikacja {
+    public static void main(String[] args) {
+    System.out.println("Zaczynamy");
+    System.out.print("Jave");
+    }
+}
