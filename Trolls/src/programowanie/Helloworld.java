53940e8883df06cd283953c563693c165d270079
Lukasz Dyrda <Lukasz.Dyrda@tzmosa.global>
2021-10-12 12:17:16 +0200

    lele

---
+++ Trolls/src/programowanie/Helloworld.java
@@ -0,0 +1,20 @@
+package programowanie;
+
+public class Helloworld {       // publiczna klasa o nazwie Helloworld i nawiaz klamrowy klasy
+
+	public static void main(String[] args) {    // definicja metody
+		System.out.println("Ja rz¹dze");     // instrukcja okreslajaca sposob dzialania metody
+		System.out.println("Œwiatem");		//do tej metody nalezy przekazywac tablice lancuchow znakow typu String
+												// a tablica ta nosi nazwe args
+		int x = 10;
+		if (x == 100) {
+			System.out.println("Oracle nie dziala");
+		}	else {
+			System.out.println("Nie mozna pomoc");
+		}
+		
+		
+				
+	}				
+
+}
